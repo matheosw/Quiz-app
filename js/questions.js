2ffbb9004693c5ecaabd7c4963f6f1ab652fc2eb
matheosw <103533878+matheosw@users.noreply.github.com>
2022-09-12 18:01:59 -0700

    built quiz app using DOM

---
+++ js/questions.js
@@ -0,0 +1,79 @@
+let MCQS = [{
+    question: "What was Quentin Tarantino's first film?",
+    choice1: "Pul fiction",
+    choice2: "Resevoir Dogs",
+    choice3: "From Dusk Til Dawn",
+    choice4: "Jackie Brown",
+    answer: 1
+},
+            {
+            question: "What is the highest grossing film of all time?",
+    choice1: "Titanic",
+    choice2: "Frozen",
+    choice3: "Avatar",
+    choice4: "Avengers Endgame",
+    answer: 2
+},
+            {
+            question: "Which actor has never won an oscar?",
+    choice1: "Matthew Mcconaughey",
+    choice2: "Johnny Depp",
+    choice3: "Sean Penn",
+    choice4: "Gary Oldman",
+    answer: 1
+},
+            {
+            question: "Which actress has never won an oscar?",
+    choice1: "Michelle Pfeiffer",
+    choice2: "Emma Stone",
+    choice3: "Cate Blanchette",
+    choice4: "Sandra Bullock",
+    answer: 0
+},
+            {
+            question: "What is the biggest movie bombs",
+    choice1: "The lone ranger",
+    choice2: "The 13th warrior",
+    choice3: "47 Ronnin",
+    choice4: "R.I.P.D",
+    answer: 1
+},
+            {
+            question: "What is the most watched movie of all time?",
+    choice1: "Lion King",
+    choice2: "Terminator",
+    choice3: "The Wizard of OZ",
+    choice4: "Star Wars",
+    answer: 2
+},
+            {
+               question: "What was the first movie to feature a toilet flushing?",
+    choice1: "Lawrence of Arabia",
+    choice2: "Casablanca",
+    choice3: "Gone with the wind",
+    choice4: "Psycho",
+    answer: 3
+},{
+               question: "In Bambi, what is the name of Bambis Skunk friend",
+    choice1: "Skunk",
+    choice2: "Smelly",
+    choice3: "Flower",
+    choice4: "Bob",
+    answer: 2
+},
+            {
+               question: "In Shrek, what does Shrek compare ogres to",
+    choice1: "Apples",
+    choice2: "Potatoes",
+    choice3: "Onions",
+    choice4: "Peaches",
+    answer: 2
+},
+            {
+               question: "In Forrest Gump, according to Forrest's mom what is life like?",
+    choice1: "A box of choclates",
+    choice2: "A shitshow",
+    choice3: "A tough journey",
+    choice4: "A loving journey",
+    answer: 0
+}];
